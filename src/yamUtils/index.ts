a1d5e42ac1dd7fa3e84dd3eef57a99b62ee27b00
Garfield Lee <openpgp@550.moe>
2021-01-26 15:32:20 +0800

    Revert "feat: update eBTCRebaser contract"
    This reverts commit 49e5226d9d6bd658339a0e7c9e83744634b24f15.

--- src/yamUtils/index.ts
+++ src/yamUtils/index.ts
@@ -229,7 +229,7 @@ export const getCurrentPriceEETH = async (yam: Yam) => {
   // return yam.toBigN(1)
 }
 export const getCurrentPriceEBTC = async (yam: Yam) => {
-  return yam.toBigN(await yam.contracts.eBTCRebaser.methods.getCurrentTWAP().call()).multipliedBy(1e10);
+  return yam.toBigN(await yam.contracts.eBTCRebaser.methods.getCurrentTWAP().call())
 }
 
 export const getTargetPrice = async (yam: Yam) => {
